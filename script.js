8e1eeb3853f9f7b55264ea6835410786b6d97cb3
Souptik Samanta <161435060+souptik-samanta@users.noreply.github.com>
2025-02-17 17:30:46 +0530

    remove the branding thing

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ const images = [
   "./img/image.png",
   "./img/wholething.png",
   "./img/image-1.png",
-  "./img/image-2.png",
   "./img/image-3.png",
   "./img/image-4.png",
   "./img/image (17).png",
